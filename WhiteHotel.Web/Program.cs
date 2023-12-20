017667a357037aac7ce8d60bc80ed175e5fad699
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-12-20 20:55:26 +0300

    #add migration EF

--- WhiteHotel.Web/Program.cs
+++ WhiteHotel.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WhiteHotel.Infrastructure.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(optionsAction =>
+    optionsAction.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
