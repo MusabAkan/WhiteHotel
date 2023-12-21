6cf97d2a4ee59793aadb0ea4684abfcfc452b2c0
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-12-21 13:48:04 +0300

    #2112231348

--- WhiteHotel.Web/Controllers/VillaController.cs
+++ WhiteHotel.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteHotel.Domain.Entities;
 using WhiteHotel.Infrastructure.Data;
 
 namespace WhiteHotel.Web.Controllers
@@ -16,6 +17,43 @@ namespace WhiteHotel.Web.Controllers
         {
             var result = _context.Villas.ToList();
             return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa model)
+        {
+            if (model.Name == model.Description)
+            {
+                ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            var result = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
         }
     }
 }
