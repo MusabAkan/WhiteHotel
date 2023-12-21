3a2b61054a13a8ec303765f1ea4ae18f356e4cbd
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-12-21 10:49:42 +0300

    #2112231049

---
+++ WhiteHotel.Web/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteHotel.Infrastructure.Data;
+
+namespace WhiteHotel.Web.Controllers
+{
+
+    public class VillaController : Controller
+    {
+        readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Villas.ToList();
+            return View(result);
+        }
+    }
+}
